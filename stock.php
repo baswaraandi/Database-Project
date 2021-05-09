e9ebafae35cb9f84b060e13a62379d1c9274ef49
baswaraandi <72914319+baswaraandi@users.noreply.github.com>
2021-05-09 23:05:01 +0700

    Add files via upload

--- stock.php
+++ stock.php
@@ -1,12 +1,77 @@
+<body bgcolor= #b8977e>
+<div style="width: 50%; margin: 0 auto; border: 2px double #FF000";
+    background-color: white; box-shadow: 1px 1px 10px 1px;">
+    <h3 align="center">Insert New Record Here </h3>
+<form action="" method="Post">
+    <fieldset>
+    <div style="padding: 10px; text-align: center;">
+          <input type="text" name="Id_Stock" value="" placeholder="Enter Id Stock Here"
+          style="padding: 5px; width : 90%">
+      </div>
+      <div style="padding: 10px; text-align: center;">
+          <input type="text" name="Jenis_Kurma" value="" placeholder="Enter Jenis Kurma Here"
+          style="padding: 5px; width : 90%">
+      </div>
+      <div style="padding: 10px; text-align: center;">
+          <input type="text" name="Qualitas_Kurma" value="" placeholder="Enter Qualitas Kurma Here"
+          style="padding: 5px; width : 90%">
+      </div>
+      <div style="padding: 10px; text-align: center;">
+          <input type="text" name="Asal_Negara" value="" placeholder="Enter Asal Negara Here"
+          style="padding: 5px; width : 90%">
+      </div>
+      <div style="padding: 10px; text-align: center;">
+          <input type="text" name="Ketersediaan_Kg" value="" placeholder="Enter Ketersediaan(KG) Kurma Here"
+          style="padding: 5px; width : 90%">
+      </div>
+      <div style="padding: 10px; text-align: center;">
+          <input type="submit" name="ins" value="Edit Data"
+          style="padding: 5px; width : 50%">
+      </div>
+      </fieldset>
+</form>
+</div>
+<?php
+if(isset($_POST['ins']))
+{
+    $con = mysqli_connect("localhost","root","admin","kurma");
+    $id = $_POST ['Id_Stock'];
+    $jk = $_POST ['Jenis_Kurma'];
+    $qk = $_POST ['Qualitas_Kurma'];
+    $an = $_POST ['Asal_Negara'];
+    $kk = $_POST ['Ketersediaan_Kg'];
+    mysqli_query($con,"insert into stock values('$id','$jk','$qk','$an','$kk')");
+    echo "<div style= 'box-shadow;  1px 1px 5px 1px rgb(255, 90, 40);'> Data Berhasil Ditambahkan.....<div>";
+}
+?>
 <?php
 $con = mysqli_connect("localhost","root","admin","kurma");
 $s=mysqli_query($con,"select * from stock");
 ?>
+<style>
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+th, td {
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even){background-color: #f2f2f2}
+
+th {
+  background-color: #4CAF50;
+  color: white;
+}
+</style>
 <table border = 5>
     <tr>
         <th>Id_Stock</th>
         <th>Jenis_Kurma</th>
         <th>Qualitas_Kurma</th>
+        <th>Asal_Negara</th>
         <th>Ketersediaan_Kg</th>
         <th>Edit<th>
     </tr>
@@ -18,6 +83,7 @@ while($r = mysqli_fetch_array($s))
         <td><?php echo $r['Id_Stock']; ?></td>
         <td><?php echo $r['Jenis_Kurma']; ?></td>
         <td><?php echo $r['Qualitas_Kurma']; ?></td>
+        <td><?php echo $r['Asal_Negara']; ?></td>
         <td><?php echo $r['Ketersediaan_Kg']; ?></td>
         <td><a href = "update.php?i=<?php echo $r['id']; ?>">Update</a></td>
     </tr>
