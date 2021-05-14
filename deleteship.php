35075933e4c12d9c701fad5bb9f55e3f918a33c4
rhezapandya <73706245+rhezapandya@users.noreply.github.com>
2021-05-14 15:30:43 +0700

    update

--- deleteship.php
+++ deleteship.php
@@ -3,7 +3,7 @@
 $id = $_GET['i'];
 echo $id;
 
-$con = mysqli_connect("localhost","root","admin","kurma");
+$con = mysqli_connect("localhost","root","","kurma");
 mysqli_query($con,"delete from ship where Id_Supplier='$id'");
 
 header("location:ship.php");
--- deletetransaksi.php
+++ deletetransaksi.php
@@ -3,7 +3,7 @@
 $id = $_GET['i'];
 echo $id;
 
-$con = mysqli_connect("localhost","root","admin","kurma");
+$con = mysqli_connect("localhost","root","","kurma");
 mysqli_query($con,"delete from transaksi where Id_Transaksi='$id'");
 
 header("location:transaksi.php");
